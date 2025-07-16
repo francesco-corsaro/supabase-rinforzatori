a97ae64bddf2a77c157c0f9403ff550aa9fb28ba
francesco-corsaro <57091974+francesco-corsaro@users.noreply.github.com>
2025-07-16 23:11:05 +0200

    Delete src directory

--- src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://mcrrafxlbcolkpfwlvzz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jcnJhZnhsYmNvbGtwZndsdnp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODY4MTEsImV4cCI6MjA2ODI2MjgxMX0.EsQsPRSbwHrmV5sg_4eeGWv877a30vRFe7S3BHj7bQk"
-);
